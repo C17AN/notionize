ee0da00216d0930361f21aedcc5ca48387a8dbda
c17an <kimchanmin1@gmail.com>
2021-07-20 13:07:07 +0900

    feat: 사용 방법 페이지 애니메이션 효과 추가

--- pages/instruction.tsx
+++ pages/instruction.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Container from "../components/layout/container";
 import ReactPageScroller from "react-page-scroller";
@@ -7,20 +7,49 @@ import Step2 from "../components/instruction/step2";
 import Step3 from "../components/instruction/step3";
 import Step4 from "../components/instruction/step4";
 import What from "../components/instruction/what";
+import { Transition } from "react-transition-group";
 
 interface Props {}
 
+const duration = 300;
+
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out`,
+  opacity: 0,
+};
+
+const transitionStyles = {
+  entering: { opacity: 1 },
+  entered: { opacity: 1 },
+  exiting: { opacity: 0 },
+  exited: { opacity: 0 },
+};
+
 const instruction = (props: Props) => {
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
   return (
-    <Container>
-      <ReactPageScroller containerHeight={"calc(100vh - 80px)"}>
-        <What />
-        <Step1 />
-        <Step2 />
-        <Step3 />
-        <Step4 />
-      </ReactPageScroller>
-    </Container>
+    <Transition in={loaded} timeout={duration}>
+      {(state: any) => (
+        <Container
+          style={{
+            ...defaultStyle,
+            ...transitionStyles[state],
+          }}
+        >
+          <ReactPageScroller containerHeight={"calc(100vh - 80px)"}>
+            <What />
+            <Step1 />
+            <Step2 />
+            <Step3 />
+            <Step4 />
+          </ReactPageScroller>
+        </Container>
+      )}
+    </Transition>
   );
 };
 
