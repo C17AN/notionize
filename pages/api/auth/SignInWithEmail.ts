2b4453d07cedcd8d1e2dde8740ccca13c5ceb82a
c17an <kimchanmin1@gmail.com>
2021-07-20 18:34:04 +0900

    feat: CORS 미들웨어 설정

--- pages/api/auth/SignInWithEmail.ts
+++ pages/api/auth/SignInWithEmail.ts
@@ -1,5 +1,7 @@
 import axios, { AxiosResponse } from "axios";
+import Cors from "cors";
 import { NextApiRequest, NextApiResponse } from "next";
+import initMiddleware from "../../../lib/init-middleware";
 
 export interface IsignInResponse {
   idToken: string;
@@ -13,7 +15,16 @@ export interface IsignInResponse {
   };
 }
 
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ["GET", "POST", "OPTIONS"],
+  })
+);
+
 const SignInWithEmail = async (req: NextApiRequest, res: NextApiResponse) => {
+  await cors(req, res);
   const { email, password } = req.body;
   const URL = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.FIREBASE_API_KEY}`;
   await axios
